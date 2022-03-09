9d766f6cacb1f75e0d31c3b4b8974020bde1be3d
sergiogomes <sergiopgomes92@gmail.com>
2022-03-09 18:10:02 -0300

    feat: :sparkles: add multiplication table view

--- controllers/tabuada.js
+++ controllers/tabuada.js
@@ -2,6 +2,13 @@ const list = (req, res) => {
   res.render('tabuada/list')
 }
 
+const table = (req, res) => {
+  res.render('tabuada/table', {
+    num: req.params.num
+  })
+}
+
 module.exports = {
-  list
+  list,
+  table,
 }
--- routes/tabuada.js
+++ routes/tabuada.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const tabuadaController = require('../controllers/tabuada')
 
 router.get('/', tabuadaController.list)
+router.get('/for/:num', tabuadaController.table)
 
 module.exports = router
