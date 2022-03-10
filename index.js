cb1c5270b475f394a1bc08ceb2aab1b38b5655b9
sergiogomes <sergiopgomes92@gmail.com>
2022-03-10 13:03:23 -0300

    feat: :sparkles: add process env port or 3000

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 3000
+const port = process.env.port || 3000
 
 const indexRouter = require('./routes/index')
 const tabuadaRouter = require('./routes/tabuada')
